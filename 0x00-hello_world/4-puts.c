70282558815fa3fa109497e2445efdca9923f01b
IamMatiasT <math.td456@gmail.com>
2023-06-16 00:15:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A code that displays a specific line using puts function
+ * return: 0 (Completed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
