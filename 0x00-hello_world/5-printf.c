3a7841f965ac3157091dd6fbf992dbe11d61e6ee
IamMatiasT <math.td456@gmail.com>
2023-06-16 03:09:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#1nclude <stdio.h>
+/**
+ * main - a program that prints text using print f
+ * Return : zero
+ * Description - print text 
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
