f1b20b8603a70dcc0fa86fd23bc48d04d936a7c1
IamMatiasT <math.td456@gmail.com>
2023-06-16 00:50:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - A code that displays a specific line using puts function
- * return: 0 (Completed)
+ * Return: zero
+ * Description - Printing a text
  */
-/*description*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
